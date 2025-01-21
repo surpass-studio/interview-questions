3a21420bbf4792ce1baeecddd8dca9d679ba51a5
Visualizeit <vnyeit@gmail.com>
2025-01-21 17:35:57 +0800

    Refactor Article component: replace CSS modules with GitHub markdown styles and update rendering logic for improved compatibility

--- app/components/question/Article.tsx
+++ app/components/question/Article.tsx
@@ -1,15 +1,24 @@
-import { TypographyStylesProvider } from '@mantine/core'
-import styles from './Article.module.css'
+import { useComputedColorScheme } from '@mantine/core'
+import darkTheme from 'github-markdown-css/github-markdown-dark.css?inline'
+import lightTheme from 'github-markdown-css/github-markdown-light.css?inline'
+import './markdown-body.css'
 
 interface ArticleProps {
 	html: string
 }
 
 const Article = ({ html }: ArticleProps) => {
+	const colorSchema = useComputedColorScheme()
+
 	return (
-		<TypographyStylesProvider className={styles.Article}>
-			<article dangerouslySetInnerHTML={{ __html: html }} />
-		</TypographyStylesProvider>
+		<>
+			<style>{colorSchema === 'light' ? lightTheme : darkTheme}</style>
+			<article
+				className="markdown-body"
+				style={{ backgroundColor: 'transparent' }}
+				dangerouslySetInnerHTML={{ __html: html }}
+			/>
+		</>
 	)
 }
 

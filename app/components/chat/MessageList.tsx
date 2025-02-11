b5bdd536df35dd43413806aaaaec6916ed70253e
Visualizeit <17688172112@163.com>
2025-02-11 22:32:48 +0800

    Refactor MessageList component: rename CSS class for user list items, enhance styling with motion animations, and update structure to use Paper and motion elements.

--- app/components/chat/MessageList.tsx
+++ app/components/chat/MessageList.tsx
@@ -1,16 +1,16 @@
 import { useChat } from '@ai-sdk/react'
 import {
 	Avatar,
-	Card,
 	Center,
 	Group,
+	Paper,
 	Stack,
 	Text,
 	ThemeIcon,
 	TypographyStylesProvider,
 } from '@mantine/core'
 import { IconSparkles } from '@tabler/icons-react'
-import clsx from 'clsx'
+import { motion } from 'motion/react'
 import { useEffect } from 'react'
 import Markdown from 'react-markdown'
 import styles from './MessageList.module.css'
@@ -46,31 +46,42 @@ const MessageList = ({ id }: MessageListProps) => {
 	}
 
 	return (
-		<Stack className="flex-1" gap="xl">
+		<Stack component="ol" className="flex-1" gap="xl">
 			{messages.map((message) => {
 				if (message.role === 'user') {
 					return (
-						<Card
+						<motion.li
 							key={message.id}
-							padding="sm"
-							className={clsx('max-w-5/6 self-end', styles.userListItem)}
+							className="max-w-5/6 self-end"
+							initial={{ y: 5, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
 						>
-							<Text>{message.content}</Text>
-						</Card>
+							<Paper p="sm" className={styles.userListItemContent}>
+								<Text>{message.content}</Text>
+							</Paper>
+						</motion.li>
 					)
 				}
 
 				return (
-					<Group key={message.id} align="start">
-						<Avatar>
-							<ThemeIcon variant="transparent">
-								<IconSparkles className="stroke-1.5" />
-							</ThemeIcon>
-						</Avatar>
-						<TypographyStylesProvider className="flex-1">
-							<Markdown className={styles.markdown}>{message.content}</Markdown>
-						</TypographyStylesProvider>
-					</Group>
+					<motion.li
+						key={message.id}
+						initial={{ y: 5, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+					>
+						<Group align="start">
+							<Avatar>
+								<ThemeIcon variant="transparent">
+									<IconSparkles className="stroke-1.5" />
+								</ThemeIcon>
+							</Avatar>
+							<TypographyStylesProvider className="flex-1">
+								<Markdown className={styles.markdown}>
+									{message.content}
+								</Markdown>
+							</TypographyStylesProvider>
+						</Group>
+					</motion.li>
 				)
 			})}
 		</Stack>

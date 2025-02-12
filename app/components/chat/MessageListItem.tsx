bc7cac4f98e7750701ea00dce83e967e0d74c51f
Visualizeit <vnyeit@gmail.com>
2025-02-12 17:14:28 +0800

    Refactor MessageList component: extract MessageListItem for better readability and maintainability, update loading state handling, and remove unused animation props

---
+++ app/components/chat/MessageListItem.tsx
@@ -0,0 +1,112 @@
+import { type UseChatHelpers } from '@ai-sdk/react'
+import {
+	Avatar,
+	Button,
+	Collapse,
+	Divider,
+	Group,
+	Loader,
+	Paper,
+	Stack,
+	Text,
+	ThemeIcon,
+	TypographyStylesProvider,
+} from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import {
+	IconChevronDown,
+	IconChevronRight,
+	IconSparkles,
+} from '@tabler/icons-react'
+import Markdown from 'react-markdown'
+import styles from './MessageListItem.module.css'
+
+interface MessageListItemProps {
+	message: UseChatHelpers['messages'][number]
+}
+
+const MessageListItem = ({ message }: MessageListItemProps) => {
+	const [isOpened, { toggle }] = useDisclosure(true)
+
+	if (message.role === 'user') {
+		return (
+			<li key={message.id} className="max-w-5/6 self-end">
+				<Paper p="sm" className={styles.userListItemContent}>
+					<Text>{message.content}</Text>
+				</Paper>
+			</li>
+		)
+	}
+
+	return (
+		<Group component="li" align="start">
+			<Avatar>
+				<ThemeIcon variant="transparent">
+					<IconSparkles className="stroke-1.5" />
+				</ThemeIcon>
+			</Avatar>
+			<Stack className="flex-1">
+				{message.parts.map((part) => {
+					if (part.type === 'reasoning') {
+						return (
+							<Stack key={part.type} align="start" gap="xs">
+								<Button
+									className="font-normal"
+									size="compact-sm"
+									color={isOpened ? undefined : 'gray'}
+									variant="subtle"
+									rightSection={
+										isOpened ? (
+											<IconChevronDown className="stroke-1.5 size-4" />
+										) : (
+											<IconChevronRight className="stroke-1.5 size-4" />
+										)
+									}
+									onClick={() => toggle()}
+								>
+									Reasoning
+								</Button>
+
+								<Collapse in={isOpened}>
+									<Group key={part.type}>
+										<Divider orientation="vertical" />
+										<TypographyStylesProvider className="flex-1" c="gray">
+											<Markdown className={styles.markdown}>
+												{part.reasoning}
+											</Markdown>
+										</TypographyStylesProvider>
+									</Group>
+								</Collapse>
+							</Stack>
+						)
+					}
+
+					if (part.type === 'text') {
+						return (
+							<TypographyStylesProvider key={part.type}>
+								<Markdown className={styles.markdown}>
+									{message.content}
+								</Markdown>
+							</TypographyStylesProvider>
+						)
+					}
+
+					return null
+				})}
+			</Stack>
+		</Group>
+	)
+}
+
+MessageListItem.Pending = () => (
+	<Group component="li">
+		<Avatar>
+			<ThemeIcon variant="transparent">
+				<IconSparkles className="stroke-1.5" />
+			</ThemeIcon>
+		</Avatar>
+		<Loader type="dots" />
+	</Group>
+)
+
+export default MessageListItem
--- app/routes/api.$/chatAPI.ts
+++ app/routes/api.$/chatAPI.ts
@@ -1,26 +1,27 @@
-import { type OpenAIProvider, createOpenAI } from '@ai-sdk/openai'
-import { type Message, streamText } from 'ai'
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
+import { type LanguageModelV1, type Message, streamText } from 'ai'
 import { Hono } from 'hono'
 import { type Bindings } from './'
 
-let openai: OpenAIProvider | null = null
+let model: LanguageModelV1 | null = null
 
 const chatAPI = new Hono<{ Bindings: Bindings }>().post('/', async (c) => {
 	const { messages } = await c.req.json<{ messages: Message[] }>()
 
-	if (openai === null) {
-		openai = createOpenAI({
-			apiKey: c.env.SILICON_CLOUD_API_KEY,
+	if (model === null) {
+		model = createOpenAICompatible({
+			name: 'SiliconFlow',
 			baseURL: 'https://api.siliconflow.cn/v1',
-		})
+			apiKey: c.env.SILICON_CLOUD_API_KEY,
+		}).chatModel('deepseek-ai/DeepSeek-R1-Distill-Qwen-7B')
 	}
 
 	const result = streamText({
-		model: openai('deepseek-ai/DeepSeek-R1-Distill-Qwen-7B'),
+		model,
 		messages,
 	})
 
-	return result.toDataStreamResponse()
+	return result.toDataStreamResponse({ sendReasoning: true })
 })
 
 export default chatAPI

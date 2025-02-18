0a821f04fbbca81e2aa9ab22f125fc12a73fb931
Visualizeit <vnyeit@gmail.com>
2025-02-18 13:34:59 +0800

    Enhance MessageTextarea and Input styling with focus outlines and transitions

--- app/configs/mantineTheme.ts
+++ app/configs/mantineTheme.ts
@@ -1,6 +1,14 @@
-import { createTheme } from '@mantine/core'
+import { createTheme, Input } from '@mantine/core'
+import styles from './components.module.css'
 
 const mantineTheme = createTheme({
+	components: {
+		Input: Input.extend({
+			classNames: {
+				input: styles.input,
+			},
+		}),
+	},
 	primaryColor: 'orange',
 	defaultRadius: 'md',
 	activeClassName: 'active:brightness-90',
